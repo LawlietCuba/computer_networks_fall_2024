35ab243fa3b4f1cfe5f1190395a373bebdf50342
LawlietCuba <rodrigao.mederos@gmail.com>
2025-02-10 13:59:53 -0500

    hell

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 # Replace the next shell command with the entrypoint of your solution
 
-python3 tests/irc/dist/IRC_Server.py
+echo "What da hell?"
--- tests/irc/tester.py
+++ tests/irc/tester.py
@@ -3,8 +3,6 @@ import subprocess
 # Path to the server executable
 server_executable_path = 'tests/irc/dist/IRC_Server.py'
 
-print("Hola bro")
-
 # Run the server executable in the background
 server_process = subprocess.Popen([server_executable_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
