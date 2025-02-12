66222c98a5142545bdb40139db24ea16ade962be
LawlietCuba <rodrigao.mederos@gmail.com>
2025-02-12 13:10:07 -0500

    One more...

--- IRC_Client.py
+++ IRC_Client.py
@@ -26,11 +26,11 @@ def main():
         sys.exit(1)
 
     try:
-        # Si el comando principal NO es NICK, primero se envía el comando NICK para identificarse.
-        if nickname not in irc_server.nicknames and command.upper() != "NICK":
-            nick_message = f"/NICK {nickname}\r\n"
-            irc_server.process_command(client_socket, nick_message)
-            irc_server.process_command(client_socket, "/JOIN #General\r\n")
+        # # Si el comando principal NO es NICK, primero se envía el comando NICK para identificarse.
+        # if nickname not in irc_server.nicknames and command.upper() != "NICK":
+        #     nick_message = f"/NICK {nickname}\r\n"
+        #     irc_server.process_command(client_socket, nick_message)
+        #     irc_server.process_command(client_socket, "/JOIN #General\r\n")
             
         time.sleep(1)
 
